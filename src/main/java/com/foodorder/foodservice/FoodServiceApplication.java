7e546369b857a22078199630408d8124396ebbe6
Jakub Wylezek <jakub.wylezek@wp.pl>
2019-08-11 15:40:49 +0200

    Create CORS handling

--- src/main/java/com/foodorder/foodservice/FoodServiceApplication.java
+++ src/main/java/com/foodorder/foodservice/FoodServiceApplication.java
@@ -12,19 +12,19 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FoodServiceApplication {
-//
-//    @Autowired
-//    private FoodRepository foodRepository;
+
+    @Autowired
+    private FoodRepository foodRepository;
 
 //    Populate mongodb
-//    @Bean
-//    CommandLineRunner preLoadMongo() {
-//        return args -> {
-//            foodRepository.save(new Food("Chicken", "So tasty", new BigDecimal(12.00)));
-//            foodRepository.save(new Food("Soup", "Very tasty", new BigDecimal(8.25)));
-//            foodRepository.save(new Food("Potato pancakes", "Mmm", new BigDecimal(4.32)));
-//        };
-//    }
+    @Bean
+    CommandLineRunner preLoadMongo() {
+        return args -> {
+            foodRepository.save(new Food("Chicken", "So tasty", new BigDecimal(12.00)));
+            foodRepository.save(new Food("Soup", "Very tasty", new BigDecimal(8.25)));
+            foodRepository.save(new Food("Potato pancakes", "Mmm", new BigDecimal(4.32)));
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FoodServiceApplication.class, args);
